2146c4fdfa980bcc807f34bf8c5b1406162bd84e
pranavpm994 <pranavpm994@gmail.com>
2023-11-23 15:34:05 +0400

    Under matintenance

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  redirects() {
+    return [
+      process.env.MAINTENANCE_MODE === "1"
+        ? { source: "/((?!maintenance).*)", destination: "/maintenance.html", permanent: false }
+        : null,
+    ].filter(Boolean);
+  }
 }
 
 module.exports = nextConfig
--- src/pages/index.js
+++ src/pages/index.js
@@ -57,7 +57,6 @@ export default function Home() {
         setIsLoading(false);
       });
   };
-  return(<div>Under Maintanence!!!</div>)
   return (
     <div className="bg-indigo-50 flex flex-col">
       <div className="flex h-[calc(100dvh)] flex-col items-center">
@@ -122,62 +121,5 @@ export default function Home() {
         </div>
       </div>
     </div>
-  );
-  return (
-    <>
-      <div className="flex-none fixed min-w-full">
-        <h1 className="bg-gradient-to-r from-blue-500 to-green-500 text-center py-3 font-bold text-6xl">
-          ChatGPT
-        </h1>
-      </div>
-      <div className="container flex flex-col min-w-full space-y-8">
-        <div className="flex space-y-4 grow overflow-y-auto flex-col p-6 pt-24 max-h-[screen] bg-gray-900">
-          {chatLog.map((message, index) => (
-            <div
-              key={index}
-              className={`flex ${
-                message.type === "user" ? "justify-end" : "justify-start"
-              }`}
-            >
-              <div
-                className={`${
-                  message.type === "user" ? "bg-purple-500" : "bg-gray-800"
-                } rounded-lg p-4 text-white max-w-sm`}
-              >
-                {message.message}
-              </div>
-            </div>
-          ))}
-          {isLoading && (
-            <div key={chatLog.length} className="flex justify-start">
-              <div className="bg-gray-800 rounded-lg p-4 text-white max-w-sm">
-                <TypingAnimation />
-              </div>
-            </div>
-          )}
-        </div>
-      </div>
-      <div className="flex-none fixed bottom-0 left-0 right-0 p-6">
-        <form onSubmit={handleSubmit}>
-          <div className="flex rounded-lg border border-gray-200 bg-gray-800">
-            <input
-              className="grow px-4 py-2 rounded-lg bg-gray-800 text-white focus:outline-none"
-              type="text"
-              placeholder="Enter some data..."
-              value={inputValue}
-              onChange={(e) => {
-                setInputValue(e.target.value);
-              }}
-            />
-            <button
-              type="submit"
-              className="flex-none bg-purple-500 rounded-lg px-4 py-2 text-white font-semibold-focus:outline-none hover:bg-purple-600 transition-colors duration-300"
-            >
-              Send
-            </button>
-          </div>
-        </form>
-      </div>
-    </>
   );
 }
